4e9d2c40ed5b63ebe2a9f17d4bb606ceeae03838
valentin-diaz <valentin.diaz@uc.cl>
2021-12-23 19:51:19 -0300

    UI basica de menu inicial

--- frontend/menu_inicial.py
+++ frontend/menu_inicial.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QLine, pyqtSignal
+from PyQt5.QtCore import QLine, pyqtSignal, Qt
 from PyQt5.QtGui import QIcon, QFont, QPixelFormat, QPixmap
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget)
 from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout)
@@ -19,5 +19,41 @@ class MenuInicial(QWidget):
         # Configuración inicial de la ventana
         self.setGeometry(*p.GEOMETRY_ARGS)
         self.setWindowTitle(p.TITULO)
+        self.setStyleSheet(f'background-color : {p.COLOR_FONDO};')
 
-        
+        # Header
+        self.contenedor_titulo = QHBoxLayout()
+        self.label_titulo = QLabel(p.STR_HEADER_INICIO, self)
+        # self.label_titulo.setStyleSheet('border : 1px solid black;')
+        self.contenedor_titulo.addStretch(1)
+        self.contenedor_titulo.addWidget(self.label_titulo)
+        self.contenedor_titulo.addStretch(1)
+
+        # Descripción
+        self.contenedor_descripcion = QHBoxLayout()
+        self.label_descripcion = QLabel(p.STR_DESCRIPCION_INICIO, self)
+        self.label_descripcion.setFixedWidth(500)
+        self.label_descripcion.setWordWrap(True)
+        self.label_descripcion.setAlignment(Qt.AlignCenter)
+        self.label_descripcion.openExternalLinks()
+        # self.label_descripcion.setStyleSheet('border : 1px solid black;')
+        self.contenedor_descripcion.addStretch(1)
+        self.contenedor_descripcion.addWidget(self.label_descripcion)
+        self.contenedor_descripcion.addStretch(1)
+
+        # Botón
+        self.contenedor_boton = QHBoxLayout()
+        self.boton = QPushButton('Iniciar', self)
+        self.contenedor_boton.addStretch(1)
+        self.contenedor_boton.addWidget(self.boton)
+        self.contenedor_boton.addStretch(1)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(2)
+        vbox.addLayout(self.contenedor_titulo)
+        vbox.addStretch(1)
+        vbox.addLayout(self.contenedor_descripcion)
+        vbox.addStretch(1)
+        vbox.addLayout(self.contenedor_boton)
+        vbox.addStretch(2)
+        self.setLayout(vbox)
--- parametros.py
+++ parametros.py
@@ -1,3 +1,12 @@
 # Parámetros iniciales de las ventanas
 GEOMETRY_ARGS = (420, 70, 600, 600)
 TITULO = 'Anime Image Generator'
+COLOR_FONDO = '#555A61'
+
+# Ventana inicial
+STR_HEADER_INICIO = '¡Bienvenido/a a Anime Image Generator!'
+STR_DESCRIPCION_INICIO = ('Esta es una pequeña aplicación en la que podrás ver y descargar imágenes'
+    ' de personajes de Anime bajo alguna de las categorías disponibles. Para más información, te '
+    'invito a visitar el repositorio de GitHub de Anime Image Generator en '
+    'https://github.com/valentin-diaz/Anime-images-generator'
+    )
